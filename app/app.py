5039c67e306ba649fdff84b66f127e7d21542712
VARU540 <159627015+VARU540@users.noreply.github.com>
2026-05-28 16:22:49 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import math 
 from flask import Flask, render_template, request 
 import pandas as pd 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer 
 from sklearn.metrics.pairwise import cosine_similarity 
 
 def prepare_data(x):
